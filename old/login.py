ea138d01c663cbf372a9bb95f121abe0a27cf4b4
mcampos-quinn <mcq@berkeley.edu>
2017-11-30 08:35:24 -0800

    cleanup

--- old/login.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-def login(destination):
-	with open("stuff.txt","r") as credentialFile:
-		credentialJSON = json.loads(credentialFile.read())
-		desiredUser = [x["user"] for x in credentialJSON if x["host"] == destination]
-		desiredCred = [y["pass"] for y in credentialJSON if y["host"] == destination]
-	return [desiredUser[0],desiredCred[0]]
