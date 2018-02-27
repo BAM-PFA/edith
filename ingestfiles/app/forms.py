dac6378628cd041f3f26f987869052987a5137d5
mcampos-quinn <mcq@berkeley.edu>
2018-02-26 16:42:59 -0800

    WIP flask version * can successfully iterate over listed items * POST data gets erased (IngestForm object gets reinitialized??) * see https://stackoverflow.com/questions/48999275/flask-dynamic-input-form-formfield-returns-blank-post-data

--- ingestfiles/app/forms.py
+++ ingestfiles/app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-import wtforms
+import wtforms,uuid
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -9,14 +9,21 @@ class LoginForm(FlaskForm):
 	submit = wtforms.SubmitField('Sign In')
 
 class ObjectForm(FlaskForm):
-	"""docstring for ObjectForm"""
+	"""
+	Fields for an individual object
+	"""
 	targetPath = wtforms.HiddenField('objectPath')
 	targetBase = wtforms.HiddenField('objectBase')
 	runIngest = wtforms.BooleanField('Ingest?')
 	doProres = wtforms.BooleanField("make prores?")
 	proresToDave = wtforms.BooleanField("deliver prores to dave?")
 	doConcat = wtforms.BooleanField("concatenate reels?")
+	uuid = wtforms.HiddenField(str(uuid.uuid4()))
 
 class IngestForm(FlaskForm):
+	'''
+	General input form
+	'''
+	choicesDict = {}
 	targetObject = wtforms.FormField(ObjectForm)
 	submit = wtforms.SubmitField('Submit')
--- ingestfiles/app/routes.py
+++ ingestfiles/app/routes.py
@@ -1,21 +1,42 @@
 from app import app, listObjects, forms
 from flask import render_template, url_for, request, redirect
+import wtforms,uuid
+from werkzeug import MultiDict
 
-@app.route('/',methods=['GET','POST'])
-@app.route('/index',methods=['GET','POST'])
+@app.route('/')#,methods=['GET','POST'])
+@app.route('/index')#,methods=['GET','POST'])
 def index():
 	DIR = app.config["SHARED_DIR"] 
 	objects = listObjects.list_objects()
+	
+	class OneObject(forms.ObjectForm):
+		# http://wtforms.simplecodes.com/docs/1.0.1/specific_problems.html
+		pass
+	choices = {}
+
+	for path,_object in objects.items():
+		choices[path] = OneObject(targetPath=path,targetBase=_object)
+
+	# print(choices)
+	for item,val in choices.items():
+		# set uuid to make sure each instance is unique... for testing only
+		setattr(val,'uuid',str(uuid.uuid4()))
+		# print(val.uuid)
+		# print(val)
+
+	# setattr(forms.IngestForm,'choicesDict',choices)
 	form = forms.IngestForm()
+	form.choicesDict = choices
+
+	# print(form.choicesDict)
 
 	if form.is_submitted():
 		print("HEY")
 		result = form.data
 		print(result)
-		# for item in form.data.items():
-		# 	print(item)
-		# print(extra)
-		# return render_template('status.html',title='Ingest',data=data,extra=extra)
+		# for key, value in result.items():
+		# 	print(key)
+		# 	print(value.data)
 
 		return redirect(url_for('status'))
 	return render_template('index.html',title='Index',objects=objects,form=form)
@@ -26,8 +47,9 @@ def status():
 	print(status)
 	try:
 		items = 'stuff'
-		data = request.form.to_dict(flat=False)
+		# data = request.form.to_dict(flat=False)
 		# extra = form.targetObject.data
+		data = request.args
 		extra = 'LKWELKLN'
 		# print(data)
 	except:
