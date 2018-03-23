af3a38a50a161861e1620f325a70ad087e5a10c0
mcampos-quinn <mcq@berkeley.edu>
2018-03-23 11:10:53 -0700

    pbcore work: * add janky instantiation subelement handling * cleanup

--- pbcore/pbcore.py
+++ pbcore/pbcore.py
@@ -29,7 +29,8 @@ class PBCoreDocument:
 		self.PBCORE_NAMESPACE = "http://www.pbcore.org/PBCore/PBCoreNamespace.html"
 		self._PBCORE = "{{}}".format(self.PBCORE_NAMESPACE)
 		self.XSI_NS = "http://www.w3.org/2001/XMLSchema-instance" 
-		self.SCHEMA_LOCATION = "http://www.pbcore.org/PBCore/PBCoreNamespace.html https://raw.githubusercontent.com/WGBH/PBCore_2.1/master/pbcore-2.1.xsd"
+		self.SCHEMA_LOCATION = ("http://www.pbcore.org/PBCore/PBCoreNamespace.html "
+			"https://raw.githubusercontent.com/WGBH/PBCore_2.1/master/pbcore-2.1.xsd")
 		# reference for namespace inclusion: 
 		# https://stackoverflow.com/questions/46405690/how-to-include-the-namespaces-into-a-xml-file-using-lxml
 		self.attr_qname = ET.QName(self.XSI_NS, "schemaLocation")
@@ -46,7 +47,10 @@ class PBCoreDocument:
 		if not pbcoreDescriptionDocumentPath:
 			self.descriptionRoot = ET.Element(
 				self._PBCORE+'pbcoreDescriptionDocument',
-				{self.attr_qname:"http://www.pbcore.org/PBCore/PBCoreNamespace.html https://raw.githubusercontent.com/WGBH/PBCore_2.1/master/pbcore-2.1.xsd"},
+				{self.attr_qname:
+					("http://www.pbcore.org/PBCore/PBCoreNamespace.html "
+					"https://raw.githubusercontent.com/WGBH/PBCore_2.1/master/pbcore-2.1.xsd")
+					},
 				nsmap=self.NS_MAP
 				)
 			self.descriptionDoc = ET.ElementTree(self.descriptionRoot)
@@ -69,13 +73,27 @@ class PBCoreDocument:
 			print('not a valid xml input ... probably?')
 			sys.exit()
 		# self.instantiation = ET.SubElement(self.descriptionRoot,'pbcoreInstantiation')
-		instantiation = self.add_SubElement(self.descriptionRoot,'pbcoreInstantiation',nsmap=self.NS_MAP)
+		instantiation = self.add_SubElement(
+			self.descriptionRoot,
+			'pbcoreInstantiation',
+			nsmap=self.NS_MAP
+			)
 		# print(self.pbcoreInstantiation.xpath('/p:pbcoreInstantiationDocument/*',namespaces=self.XPATH_NS_MAP))
-		for element in pbcoreInstantiation.xpath('/p:pbcoreInstantiationDocument/*',namespaces=self.XPATH_NS_MAP):
+		for element in pbcoreInstantiation.xpath(
+			'/p:pbcoreInstantiationDocument/*',
+			namespaces=self.XPATH_NS_MAP
+			):
 			# print(element.tag)
 			instantiation.insert(0,deepcopy(element))
 
-	def add_SubElement(self,_parent,_tag,attrib={},_text=None,nsmap=None,**_extra):
+	def add_SubElement(
+		self,
+		_parent,
+		_tag,attrib={},
+		_text=None,
+		nsmap=None,
+		**_extra
+		):
 		# e.g. >> sample.add_SubElement(
 		#							sample.descriptionRoot,
 		#							'pbcoreSub',{},'HELLO',
@@ -95,6 +113,22 @@ class PBCoreDocument:
 		# there should be only one asset
 		asset = list(descriptiveJSON.keys())[0]
 		assetBasename = descriptiveJSON[asset]['basename']
+		# if a field applies to an instantiation, 
+		# search for the right pbcoreInstantiation
+		# by basename and insert the element.
+		# currently something is up with my namespace
+		# assignment. so no 'p:' for my elements, 
+		# but need to use 'p:' for mediainfo elements.
+		instantiationXpathExpression = (
+			"/pbcoreDescriptionDocument/pbcoreInstantiation["
+				"contains(./p:instantiationIdentifier,'{}')"
+					"]".format(
+						assetBasename)
+			)
+		targetInstantiation = self.descriptionRoot.xpath(
+			instantiationXpathExpression,
+			namespaces=self.XPATH_NS_MAP
+			)
 		# print(assetBasename)
 		# grab the metadata dict from the JSON file
 		metadata = descriptiveJSON[asset]['metadata']
@@ -135,20 +169,22 @@ class PBCoreDocument:
 					value to the pbcore element.
 					'''
 					if level == "WORK":
-						self.add_SubElement(self.descriptionRoot,mappingTarget,attrib=mappingAttribs,_text=mdValue,nsmap=self.NS_MAP)
+						self.add_SubElement(
+							self.descriptionRoot,
+							mappingTarget,
+							attrib=mappingAttribs,
+							_text=mdValue,
+							nsmap=self.NS_MAP
+							)
 					else:
-						# if it applies to an instantiation, 
-						# search for the right pbcoreInstantiation
-						# by basename and insert the element.
-						# currently something is up with my namespace
-						# assignment. so no 'p:' for my elements, 
-						# but need to use 'p:' for mediainfo elements.
-						xpathExpression = "/pbcoreDescriptionDocument/pbcoreInstantiation[contains(./p:instantiationIdentifier,'{}')]".format(assetBasename)
-						# print(xpathExpression)
-						targetInstantiation = self.descriptionRoot.xpath(xpathExpression,namespaces=self.XPATH_NS_MAP)
-						# print(targetInstantiation)
 						if not targetInstantiation == []:
-							self.add_SubElement(targetInstantiation[0],mappingTarget,attrib=mappingAttribs,_text=mdValue,nsmap=self.NS_MAP)
+							self.add_SubElement(
+								targetInstantiation[0],
+								mappingTarget,
+								attrib=mappingAttribs,
+								_text=mdValue,
+								nsmap=self.NS_MAP
+								)
 					if "SIBLING_FIELDS" in mappedPbcore:
 						# this might not really be needed...
 						for key, value in mappedPbcore["SIBLING_FIELDS"].items():
@@ -158,27 +194,68 @@ class PBCoreDocument:
 							else:
 								attrib = {}
 							text = subSib["TEXT"]
-							self.add_SubElement(self.descriptionRoot,key,attrib=attrib,_text=text,nsmap=self.NS_MAP)
+							self.add_SubElement(
+								self.descriptionRoot,
+								key,
+								attrib=attrib,
+								_text=text,
+								nsmap=self.NS_MAP
+								)
 				else:
 					'''
 					If the meat is in a SubElement
 					add the relevant subelement(s)
 					'''
 					if level == "WORK":
-						top = self.add_SubElement(self.descriptionRoot,mappingTarget,attrib=mappingAttribs,nsmap=self.NS_MAP)
-						print(top)
+						top = self.add_SubElement(
+							self.descriptionRoot,
+							mappingTarget,
+							attrib=mappingAttribs,
+							nsmap=self.NS_MAP
+							)
+						# print(top)
 						for key,value in mappedPbcore["SUBELEMENTS"].items():
-							print(key)
+							# print(key)
 							if "ATTRIBUTES" in mappedPbcore["SUBELEMENTS"][key]:
 								attrib = mappedPbcore["SUBELEMENTS"][key]["ATTRIBUTES"]
 							else:
 								attrib = {}
-							subelement = self.add_SubElement(top,key,attrib=attrib,nsmap=self.NS_MAP)
-							print(subelement)
+							subelement = self.add_SubElement(
+								top,
+								key,
+								attrib=attrib,
+								nsmap=self.NS_MAP
+								)
+							# print(subelement)
 							if mappedPbcore["SUBELEMENTS"][key]["TEXT"] == "value":
 								subelement.text = mdValue
 							else:
 								subelement.text = mappedPbcore["SUBELEMENTS"][key]["TEXT"]
+					else:
+						if not targetInstantiation == []:
+							top = self.add_SubElement(
+								targetInstantiation[0],
+								mappingTarget,
+								attrib=mappingAttribs,
+								nsmap=self.NS_MAP
+								)
+							for key,value in mappedPbcore["SUBELEMENTS"].items():
+								# print(key)
+								if "ATTRIBUTES" in mappedPbcore["SUBELEMENTS"][key]:
+									attrib = mappedPbcore["SUBELEMENTS"][key]["ATTRIBUTES"]
+								else:
+									attrib = {}
+								subelement = self.add_SubElement(
+									top,
+									key,
+									attrib=attrib,
+									nsmap=self.NS_MAP
+									)
+								# print(subelement)
+								if mappedPbcore["SUBELEMENTS"][key]["TEXT"] == "value":
+									subelement.text = mdValue
+								else:
+									subelement.text = mappedPbcore["SUBELEMENTS"][key]["TEXT"]
 
 
 	def to_string(self):
@@ -188,5 +265,9 @@ class PBCoreDocument:
 	def xml_to_file(self,outputPath):
 		with open(outputPath,'wb') as outXML:
 			# self.output = ET.ElementTree(self.descriptionRoot)
-			self.descriptionDoc.write(outXML, encoding='utf-8', xml_declaration=True,pretty_print=True)
+			self.descriptionDoc.write(
+				outXML, 
+				encoding='utf-8', 
+				xml_declaration=True,
+				pretty_print=True)
 
