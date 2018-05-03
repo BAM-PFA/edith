4b27ba5f9d44eb24199a4e4d4faf4ecd61f19537
mcampos-quinn <mcq@berkeley.edu>
2018-05-03 11:57:37 -0700

    LTO STUFF: * work on  form for adding a new LTO ID for the 'A' tape

--- ingestfiles/app/lto/views.py
+++ ingestfiles/app/lto/views.py
@@ -2,6 +2,7 @@
 # standard library modules
 import json
 import os
+import re
 import sys
 import urllib
 import uuid
@@ -16,6 +17,14 @@ from .. import listObjects
 from .. import utils
 
 
+# we're going to store the current LTO id for the "A" tape in a text file.
+# get the value of that ID for processing/display/mounting LTO later.
+tmpDir = utils.get_temp_dir()
+ltoIdFilePath = os.path.join(tmpDir,'LTOID.txt')
+if not os.path.exists(ltoIdFilePath):
+	with open(ltoIdFilePath,'w') as idfile:
+		idfile.write('no lto id in use')
+
 @lto.route('/lto_menu',methods=['GET','POST'])
 def lto_menu():
 
@@ -27,13 +36,6 @@ def lto_menu():
 
 @lto.route('/format_lto',methods=['GET','POST'])
 def format_lto():
-	# we're going to store the current LTO id for the "A" tape in a text file.
-	# get the value of that ID for processing/display/mounting LTO later.
-	tmpDir = utils.get_temp_dir()
-	ltoIdFilePath = os.path.join(tmpDir,'LTOID.txt')
-	if not os.path.exists(ltoIdFilePath):
-		with open(ltoIdFilePath,'w') as idfile:
-			idfile.write('no lto id in use')
 	with open(ltoIdFilePath,'r') as idfile:
 		currentLTOid = idfile.readline().strip()
 
@@ -48,7 +50,24 @@ def format_lto():
 
 @lto.route('/lto_id',methods=['GET','POST'])
 def lto_id():
+	tapeIdRegex = re.compile(r'^((\d{4}[A-Z]A)|(\d{5}A))$')
+	ltoIDstatus = False
 	try:
 		_data = request.form.to_dict(flat=False)
 		ltoID = request.form['tapeAid']
+		if re.match(tapeIdRegex,ltoID):
+			with open(ltoIdFilePath,'w') as idfile:
+				currentLTOid = idfile.write(ltoID)
+			ltoIDstatus = True
+		else:
+			ltoIDstatus = False
+	except:
+		_data = 'none'
+		ltoID = 'none'
+
+	return render_template(
+		'lto_id.html',
+		ltoID=ltoID,
+		ltoIDstatus = ltoIDstatus
+		)
 
