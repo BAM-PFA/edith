6f8c6c1a6115699e117d9ee014caa08aa0f767ef
mcampos-quinn <mcq@berkeley.edu>
2018-10-06 22:39:13 -0700

    add metadata subform stub and start work on UI

--- edith/app/ingest/forms.py
+++ edith/app/ingest/forms.py
@@ -2,6 +2,13 @@ from flask_wtf import FlaskForm
 import wtforms
 from wtforms.validators import DataRequired, Email
 
+class MetadataForm(FlaskForm):
+        '''
+        Fields for additional metadata
+        '''
+        metadataTest1 = wtforms.StringField("one metadata?",default='')
+        metadataTest2 = wtforms.StringField("two metadata?",default='')
+
 class ObjectForm(FlaskForm):
 	"""
 	Fields for an individual object
@@ -12,6 +19,8 @@ class ObjectForm(FlaskForm):
 	doProres = wtforms.BooleanField("make prores?",default='')
 	proresToDave = wtforms.BooleanField("deliver prores to dave?",default='')
 	doConcat = wtforms.BooleanField("concatenate reels?",default='')
+	metadataForm = wtforms.FormField(MetadataForm)
+	#metadataFields = wtforms.FieldList(StringField('metadata1'))
 
 class IngestForm(FlaskForm):
 	'''
