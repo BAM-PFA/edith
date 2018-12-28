1bfc200ef9f5c209c92066e159da900fb07b7c6a
mcampos-quinn <mcq@berkeley.edu>
2018-12-28 14:01:02 -0800

    change deptName db field; can now add and edit users and departments

--- edith/app/admin/views.py
+++ edith/app/admin/views.py
@@ -9,9 +9,9 @@ from flask import abort, flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from . import admin
-from .forms import DepartmentForm
+from .forms import DepartmentForm, AddUserForm, EditUserForm
 from .. import db
-from ..models import Department
+from ..models import Department, User
 
 
 def check_admin():
@@ -33,9 +33,11 @@ def list_departments():
 
 	departments = Department.query.all()
 
-	return render_template('admin/departments/departments.html',
-						   departments=departments, title="Departments")
-
+	return render_template(
+		'admin/departments/departments.html',
+		departments=departments,
+		title="Departments"
+		)
 
 @admin.route('/departments/add', methods=['GET', 'POST'])
 @login_required
@@ -49,8 +51,10 @@ def add_department():
 
 	form = DepartmentForm()
 	if form.validate_on_submit():
-		department = Department(name=form.name.data,
-								description=form.description.data)
+		department = Department(
+			name=form.deptname.data,
+			description=form.description.data
+			)
 		try:
 			# add department to the database
 			db.session.add(department)
@@ -82,7 +86,7 @@ def edit_department(id):
 	department = Department.query.get_or_404(id)
 	form = DepartmentForm(obj=department)
 	if form.validate_on_submit():
-		department.name = form.name.data
+		department.deptname = form.deptname.data
 		department.description = form.description.data
 		db.session.commit()
 		flash('You have successfully edited the department.')
@@ -91,10 +95,15 @@ def edit_department(id):
 		return redirect(url_for('admin.list_departments'))
 
 	form.description.data = department.description
-	form.name.data = department.name
-	return render_template('admin/departments/department.html', action="Edit",
-						   add_department=add_department, form=form,
-						   department=department, title="Edit Department")
+	form.deptname.data = department.deptname
+	return render_template(
+		'admin/departments/department.html', 
+		action="Edit",
+		add_department=add_department, 
+		form=form,
+		department=department,
+		title="Edit Department"
+		)
 
 
 @admin.route('/departments/delete/<int:id>', methods=['GET', 'POST'])
@@ -114,3 +123,139 @@ def delete_department(id):
 	return redirect(url_for('admin.list_departments'))
 
 	return render_template(title="Delete Department")
+
+####################
+# User Views
+@admin.route('/users')
+@login_required
+def list_users():
+    """
+    List all users
+    """
+    check_admin()
+
+    users = User.query.all()
+    return render_template('admin/users/users.html',
+                           users=users, title='Users')
+
+@admin.route('/users/add', methods=['GET', 'POST'])
+@login_required
+def add_user():
+	"""
+	Add a user to the database
+	"""
+	check_admin()
+
+	add_user = True
+
+	form = AddUserForm()
+	# print(form.data)
+	print("HIIHII")
+	if form.validate_on_submit():
+		print(form.department_id.data.id)
+		print(type(form.department_id.data))
+
+		user = User(
+			department_id=form.department_id.data.id,
+			email=form.email.data,
+			username=form.username.data,
+			first_name=form.first_name.data,
+			last_name=form.last_name.data,
+			RSusername=form.RSusername.data,
+			RSkey=form.RSkey.data,
+			is_admin=form.is_admin.data,
+			password=form.password.data
+			)
+		try:
+			# add department to the database
+			db.session.add(user)
+			print(user)
+			db.session.commit()
+			flash('You have successfully added a new user.')
+		except Exception as e:
+			# in case department name already exists
+			# print(str(e))
+			flash('Error: user already exists.')
+
+		# redirect to departments page
+		return redirect(url_for('admin.list_users'))
+
+	# load department template
+	return render_template(
+		'admin/users/user.html',
+		action="Add",
+		add_user=add_user,
+		form=form,
+		title="Add User"
+		)
+
+@admin.route('/users/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_user(id):
+	"""
+	Edit a user
+	""" 
+	check_admin()
+
+	add_user = False
+
+	user = User.query.get_or_404(id)
+	form = EditUserForm(obj=user)
+	if form.validate_on_submit():
+		print(form.data)
+		user.department_id=form.department_id.data.id,
+		user.email = form.email.data
+		user.username = form.username.data
+		user.first_name = form.first_name.data
+		user.last_name = form.last_name.data
+		user.RSusername = form.RSusername.data
+		user.RSkey = form.RSkey.data
+		user.is_admin = form.is_admin.data
+		if form.password.data:
+			user.password = form.password.data
+		try:
+			db.session.commit()
+			print(form.data)
+			flash('You have successfully edited the user.')
+
+			# redirect to the users page
+			return redirect(url_for('admin.list_users'))
+		except Exception as e:
+			print(e)
+			flash('Error editing the user.')
+			return redirect(url_for('admin.list_users'))
+
+	# this pre-populates the form with existing data from the db
+	form.email.data = user.email
+	form.username.data = user.username
+	form.first_name.data = user.first_name
+	form.last_name.data = user.last_name 
+	form.RSusername.data = user.RSusername
+	form.RSkey.data = user.RSkey
+	form.is_admin.data = user.is_admin
+	return render_template(
+		'admin/users/user.html', 
+		action="Edit",
+		add_user=add_user, 
+		form=form,
+		user=user,
+		title="Edit User"
+		)
+
+@admin.route('/users/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_user(id):
+	"""
+	Delete a user from the database
+	"""
+	check_admin()
+
+	user = User.query.get_or_404(id)
+	db.session.delete(user)
+	db.session.commit()
+	flash('You have successfully deleted the user.')
+
+	# redirect to the users page
+	return redirect(url_for('admin.list_users'))
+
+	return render_template(title="Delete User")
