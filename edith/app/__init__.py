1a0f6210836be6d328822f949f8ccc6f26dcb690
mcampos-quinn <mcq@berkeley.edu>
2018-12-27 17:50:53 -0800

    start pasting in admin user functionality

--- edith/app/__init__.py
+++ edith/app/__init__.py
@@ -41,10 +41,13 @@ def create_app(config_name):
 	from app import models
 
 	from .admin import admin as admin_blueprint
-	app.register_blueprint(admin_blueprint, url_prefix='/admin')
+	app.register_blueprint(admin_blueprint)
 
 	from .auth import auth as auth_blueprint
 	app.register_blueprint(auth_blueprint)
+
+	from .home import home as home_blueprint
+	app.register_blueprint(home_blueprint)#, url_prefix='/home')
 
 	from .ingest import ingest as ingest_blueprint
 	app.register_blueprint(ingest_blueprint)
--- edith/app/admin/forms.py
+++ edith/app/admin/forms.py
@@ -1,13 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-from ..models import User
 
-# class LoginForm(FlaskForm):
-#	 """
-#	 Form for users to login
-#	 """
-#	 email = StringField('Email', validators=[DataRequired(), Email()])
-#	 password = PasswordField('Password', validators=[DataRequired()])
-#	 submit = SubmitField('Login')
+class DepartmentForm(FlaskForm):
+    """
+    Form for admin to add or edit a department
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
