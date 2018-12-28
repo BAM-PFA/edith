839ad08361e6e3387d6584d971d4de47ce4a129f
mcampos-quinn <mcq@berkeley.edu>
2018-12-28 14:41:23 -0800

    can add and edit data sources in db

--- edith/app/admin/forms.py
+++ edith/app/admin/forms.py
@@ -16,11 +16,11 @@ class DepartmentForm(FlaskForm):
 
 class AddUserForm(FlaskForm):
 	"""
-	Form for admin to create or edit users
+	Form for admin to create users
 	"""
 	department_id = QuerySelectField(
 		query_factory=lambda: Department.query.all(),
-        get_pk=lambda x: x.id,
+		get_pk=lambda x: x.id,
 		get_label="deptname"
 		)
 	email = StringField('email address', validators=[DataRequired(),Email()])
@@ -40,25 +40,37 @@ class AddUserForm(FlaskForm):
 			raise ValidationError('Email is already in use.')
 
 class EditUserForm(FlaskForm):
-    """
-    Form for admin to create or edit users
-    """
-    department_id = QuerySelectField(
-        query_factory=lambda: Department.query.all(),
-        get_label="deptname"
-        )
-    email = StringField('email address', validators=[DataRequired(),Email()])
-    username = StringField('username', validators=[DataRequired()])
-    first_name = StringField('First name', validators=[DataRequired()])
-    last_name = StringField('Last name', validators=[DataRequired()])
-    RSusername = StringField('ResourceSpace username', validators=[DataRequired()])
-    RSkey = StringField('ResourceSpace API key', validators=[DataRequired()])
-    is_admin = BooleanField('Admin?')
-    password = PasswordField('Password', validators=[EqualTo('confirm_password')])
-    confirm_password = PasswordField('Confirm Password')
+	"""
+	Form for admin to create or edit users
+	Had to create a separate form to avoid the validate_email method above, 
+	otherwise editing an existing user will fail. 
+	There's a better way to do it I know but I;m in a hurry. :/
+	"""
+	department_id = QuerySelectField(
+		query_factory=lambda: Department.query.all(),
+		get_label="deptname"
+		)
+	email = StringField('email address', validators=[DataRequired(),Email()])
+	username = StringField('username', validators=[DataRequired()])
+	first_name = StringField('First name', validators=[DataRequired()])
+	last_name = StringField('Last name', validators=[DataRequired()])
+	RSusername = StringField('ResourceSpace username', validators=[DataRequired()])
+	RSkey = StringField('ResourceSpace API key', validators=[DataRequired()])
+	is_admin = BooleanField('Admin?')
+	password = PasswordField('Password', validators=[EqualTo('confirm_password')])
+	confirm_password = PasswordField('Confirm Password')
 
-    submit = SubmitField('Submit')
+	submit = SubmitField('Submit')
 
-    # def validate_email(self, field):
-    #     if User.query.filter_by(email=field.data).first():
-    #         raise ValidationError('Email is already in use.')
+class DataSourceForm(FlaskForm):
+	"""
+	Form for admin to create or edit users
+	"""
+	dbName = StringField('Database name', validators=[DataRequired()])
+	fmpLayout = StringField('FileMaker Layout name')
+	IPaddress = StringField('IP Address', validators=[DataRequired()])
+	username = StringField("Database user's username", validators=[DataRequired()])
+	credentials = StringField('Database user password', validators=[DataRequired()])
+	description = StringField('Database description')
+
+	submit = SubmitField('Submit')
