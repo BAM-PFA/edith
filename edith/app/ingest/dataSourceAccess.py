20ec47eb42c796dc0c8957eb0178b4a57a47cec4
mcampos-quinn <mcq@berkeley.edu>
2019-02-07 11:54:47 -0800

    add draft of data source details for external metadata query

---
+++ edith/app/ingest/dataSourceAccess.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# local imports
+from .. models import Data_Source
+
+
+def main(dataSourceID):
+	dataSourceID = int(dataSourceID)
+	dataSource = Data_Source.query.get(dataSourceID)
+
+	results = {
+		'dataSourceName' : dataSource.dbName,
+		'dataSourceLayout' : dataSource.fmpLayout,
+		'dataSourceIP' : dataSource.IPaddress,
+		'dataSourceUsername' : dataSource.username,
+		'dataSourceCredentials' : dataSource.credentials
+	}
+
+	return results
+
